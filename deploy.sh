221b0b87e3cf2c8f7442ad9fd8dbfcbbf8dafb8a
无声 <ivestszheng@qq.com>
2022-06-10 15:42:21 +0800

    docs

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git checkout gh-pages
+git checkout origin/gh-pages
 git push -f git@github.com:ivestszheng/blog.git gh-pages
 
 cd -
