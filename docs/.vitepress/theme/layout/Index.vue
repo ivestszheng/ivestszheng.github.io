e593f64b050e5c717be2325b59251921a163dbd2
无声2017 <ivestszheng@qq.com>
2024-03-26 15:40:17 +0800

    fix(gitalk): 给评论组件添加 key 值，防止不刷新

--- docs/.vitepress/theme/layout/Index.vue
+++ docs/.vitepress/theme/layout/Index.vue
@@ -1,14 +1,16 @@
 <script setup>
 import Theme from 'vitepress/theme'
 import Comment from './Comment.vue'
+import { useRoute } from 'vitepress'
 
 const { Layout } = Theme
+const route = useRoute()
 </script>
 <template>
   <Layout>
     <!-- #doc-after 表示在每篇文章的最后位置添加 Comment组件 -->
     <template #doc-after>
-      <Comment />
+      <Comment :key="route.path" />
     </template>
   </Layout>
 </template>
