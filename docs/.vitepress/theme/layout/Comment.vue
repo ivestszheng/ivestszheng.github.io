6ea18e5f4f7424facb098beeb7ecaccee98263b0
无声2017 <ivestszheng@qq.com>
2024-03-27 09:20:14 +0800

    refactor(gitalk): 使用纯净 url，以文件名生成 id，而不是路径

--- docs/.vitepress/theme/layout/Comment.vue
+++ docs/.vitepress/theme/layout/Comment.vue
@@ -10,19 +10,23 @@ const commentRef = ref<HTMLElement | null>(null)
 const init = () => {
     try {
         if (inBrowser) {
-        const gitalk = new Gitalk({
-            id: md5(location.pathname), // 可选。默认为 location.href
-            owner: 'ivestszheng', // GitHub repository 所有者
-            repo: 'blog', // GitHub repo
-            clientID: '5e2f51071c7c5437e7a5', // clientID
-            clientSecret: 'd95b516545c0233ef405eabe7b99cba43bf8da27', // clientSecret
-            admin: ['ivestszheng'], // GitHub repo 所有者
-            labels: ['Gitalk'], // GitHub issue 标签
-            proxy: 'https://mellifluous-bombolone-049a57.netlify.app/github_access_token',
-            createIssueManually: false //如果当前页面没有相应的 issue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
-        })
-        gitalk.render('gitalk-page-container')
-    }
+            const fullPath = window.location.pathname,
+                segments = fullPath.split('/'),
+                lastSegment = segments[segments.length - 1];
+
+            const gitalk = new Gitalk({
+                id: md5(lastSegment),
+                owner: 'ivestszheng',
+                repo: 'blog',
+                clientID: '5e2f51071c7c5437e7a5',
+                clientSecret: 'd95b516545c0233ef405eabe7b99cba43bf8da27',
+                admin: ['ivestszheng'],
+                labels: ['Gitalk'],
+                proxy: 'https://mellifluous-bombolone-049a57.netlify.app/github_access_token',
+                createIssueManually: false //如果当前页面没有相应的 issue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
+            })
+            gitalk.render('gitalk-page-container')
+        }
     } catch (error) {
         console.error(error)
     }
