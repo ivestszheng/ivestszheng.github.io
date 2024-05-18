a0b52917edb2cdcecbba10f8ab3e3382b6149a52
无声 <ivestszheng@qq.com>
2024-05-18 21:48:15 +0800

    fix: 修复了 docs/.vitepress theme 路径下组件样式不生效的问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./docs/**/*.js",
-    "./docs/**/*.ts",
-    "./docs/**/*.vue",
-    "./docs/**/*.md",
+    "./docs/**/*.{html,js,vue,ts,md}",
+    "./docs/.vitepress/**/*.{html,js,vue,ts,md}",
   ],
   options: {
     safelist: ["html", "body"],
   },
-}
-
+};
